435bf0a237b713ea79e891b6f8e2cb9e7dc9f921
Daniel <dktdaniel@gmail.com>
2017-09-01 14:36:39 -0700

    Began implementation

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,51 @@
-// YOUR CODE HERE:
+class App {
+	constructor(name, message, server) {
+		this.name = name;
+		this.message = message;
+		this.server = 'http://parse.sfm6.hackreactor.com';
+	}
+	init() {
+		
+	}
+	send(message) {
+		$.ajax({
+			  // This is the url you should use to communicate with the parse API server.
+			  url: 'http://parse.sfm6.hackreactor.com/',
+			  type: 'POST',
+			  data: message,
+			  contentType: 'application/json',
+			  success: function (data) {
+			    console.log('chatterbox: Message sent');
+			  },
+			  error: function (data) {
+			    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+			    console.error('chatterbox: Failed to send message', data);
+			  }
+		});
+	}
+	fetch() {
+		//put GET request here!!!!
+		$.ajax({
+			  // This is the url you should use to communicate with the parse API server.
+			  url: 'http://parse.sfm6.hackreactor.com',
+			  type: 'GET',
+			  data: JSON.stringify(message),
+			  contentType: 'application/json',
+			  success: function (data) {
+			    console.log('chatterbox: Message sent');
+			  },
+			  error: function (data) {
+			    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+			    console.error('chatterbox: Failed to send message', data);
+			  }
+
+		});
+		$('#first').text('TESTING JQUERY!!! IMCHANGING UR TEXT LOLLL');
+	}
+	clearMessages(){}
+	renderMessage(){}
+	renderRoom(){}
+}
+
+
+var app = new App();
